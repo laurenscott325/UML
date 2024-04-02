0cf6f1d5ea1c36ab0663c3c3cf21eb153d68831a
laurenscott325 <130198217+laurenscott325@users.noreply.github.com>
2024-04-02 19:50:46 -0400

    Create User.java

---
+++ User.java
@@ -0,0 +1,37 @@
+import java.io.Serializable;
+
+public abstract class User implements Serializable {
+    private String name;
+    private String username;
+    private String password;
+
+    public User(String name, String username, String password) {
+        this.name = name;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
